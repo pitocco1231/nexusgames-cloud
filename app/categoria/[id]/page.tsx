2008ec5783a8d44a8298d0aa9eedfb3b94377251
pitocco1231 <144061281+pitocco1231@users.noreply.github.com>
2026-09-12 19:06:33 -0300

    style: use real artwork on category pages

--- app/categoria/[id]/page.tsx
+++ app/categoria/[id]/page.tsx
@@ -3,13 +3,13 @@ import { products } from "../../../lib/catalog";
 import { getLiveOptionsForCategory } from "../../../lib/liveStore";
 
 const images: Record<string, string> = {
-  "mobile-legends": "/assets/mobile-legends",
-  playstation: "/assets/playstation",
-  xbox: "/assets/xbox",
-  minecraft: "/assets/minecraft",
-  roblox: "/assets/roblox",
-  valorant: "/assets/valorant",
-  steam: "/assets/steam"
+  "mobile-legends": "https://epngame.com/images/games/mlbb-big-1720743035-6690747b26195.png",
+  playstation: "https://www.pakdukaan.pk/cdn/shop/files/PlayStation-Store-PSN-Gift-Card-Price-in-Pakistan-2_1370x.jpg?v=1731663639",
+  xbox: "https://www.nme.com/wp-content/uploads/2025/05/Xbox-Game-Pass-Key-Art-696x442.jpg",
+  minecraft: "https://cdn.mos.cms.futurecdn.net/v2/t%3A0%2Cl%3A448%2Ccw%3A1152%2Cch%3A1152%2Cq%3A80%2Cw%3A1152/rpPGiw7RjFaeJCCDBC4Bna.jpg",
+  roblox: "https://store-images.s-microsoft.com/image/apps.58700.68327322396008232.bebf9df1-1c64-4f6c-b0af-31eb22f07ff3.ca76e906-0122-4627-925b-3feb7422154f",
+  valorant: "https://wegame.gtimg.com/g.2001715-r.9a324/info/1d8ac7c7cdcf660f503405796c7d0d83.jpg/1000",
+  steam: "https://www.allkeyshop.com/blog/wp-content/uploads/store_steam_featured.jpg"
 };
 
 export const dynamic = "force-dynamic";
@@ -30,7 +30,7 @@ export default async function CategoryPage({ params }: { params: Promise<{ id: s
       </header>
 
       <section className="categoryHero">
-        <img src={images[id]} alt={product.name} />
+        <img src={images[id]} alt={product.name} className="categoryBackdropImage" />
         <div className="categoryHeroShade" />
         <div className="categoryHeroContent">
           <span className="heroBadge"><i /> CATÁLOGO ATUALIZADO</span>
