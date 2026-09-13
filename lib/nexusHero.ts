e6f9779b1361d72b313591d3e2c9261c4de715b0
pitocco1231 <144061281+pitocco1231@users.noreply.github.com>
2026-09-13 00:43:40 -0300

    feat: assemble custom NexusGames hero artwork

---
+++ lib/nexusHero.ts
@@ -0,0 +1,8 @@
+import { heroChunk1 } from "./nexusHeroChunk1";
+import { heroChunk2 } from "./nexusHeroChunk2";
+import { heroChunk3 } from "./nexusHeroChunk3";
+import { heroChunk4 } from "./nexusHeroChunk4";
+import { heroChunk5 } from "./nexusHeroChunk5";
+import { heroChunk6 } from "./nexusHeroChunk6";
+
+export const NEXUS_HERO_BACKGROUND = `data:image/webp;base64,${heroChunk1}${heroChunk2}${heroChunk3}${heroChunk4}${heroChunk5}${heroChunk6}`;
